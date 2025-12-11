f7dc8575deb1b72bcf482f827c72f8adbb0d00eb
Balesz01 <144881386+Balesz01@users.noreply.github.com>
2025-12-11 20:24:20 +0100

    a html és sql kijavítása

--- Kalaplengeto/kalaplengeto.sql
+++ Kalaplengeto/kalaplengeto.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1
--- Létrehozás ideje: 2025. Dec 11. 15:49
+-- Létrehozás ideje: 2025. Dec 11. 20:21
 -- Kiszolgáló verziója: 10.4.32-MariaDB
 -- PHP verzió: 8.2.12
 
@@ -24,94 +24,48 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Tábla szerkezet ehhez a táblához `attempt`
+-- Tábla szerkezet ehhez a táblához `versenyzok`
 --
 
-CREATE TABLE `attempt` (
+CREATE TABLE `versenyzok` (
   `id` int(11) NOT NULL,
-  `competitor_id` int(11) NOT NULL,
-  `round` tinyint(4) NOT NULL CHECK (`round` between 1 and 3),
-  `score` tinyint(4) NOT NULL CHECK (`score` between 0 and 10),
-  `time_ms` int(11) DEFAULT NULL
+  `nev` varchar(100) NOT NULL,
+  `lengetes1` tinyint(4) NOT NULL,
+  `ido1` double NOT NULL,
+  `lengetes2` tinyint(4) NOT NULL,
+  `ido2` double NOT NULL,
+  `lengetes3` tinyint(4) NOT NULL,
+  `ido3` double NOT NULL,
+  `legjobb_pont` int(11) NOT NULL,
+  `legjobb_ido` double NOT NULL,
+  `pilla_hely` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
 
 --
--- A tábla adatainak kiíratása `attempt`
+-- A tábla adatainak kiíratása `versenyzok`
 --
 
-INSERT INTO `attempt` (`id`, `competitor_id`, `round`, `score`, `time_ms`) VALUES
-(1, 1, 1, 9, 1620),
-(2, 1, 2, 8, 5000),
-(3, 1, 3, 10, 1050),
-(4, 2, 1, 10, 1100),
-(5, 2, 2, 6, 4500),
-(6, 2, 3, 7, 3000),
-(7, 3, 1, 8, 2200),
-(8, 3, 2, 8, 2300),
-(9, 3, 3, 9, 980);
-
--- --------------------------------------------------------
-
---
--- Tábla szerkezet ehhez a táblához `competitor`
---
-
-CREATE TABLE `competitor` (
-  `id` int(11) NOT NULL,
-  `name` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
-
---
--- A tábla adatainak kiíratása `competitor`
---
-
-INSERT INTO `competitor` (`id`, `name`) VALUES
-(1, 'Kovács János'),
-(2, 'Nagy Béla'),
-(3, 'Szabó Erika');
+INSERT INTO `versenyzok` (`id`, `nev`, `lengetes1`, `ido1`, `lengetes2`, `ido2`, `lengetes3`, `ido3`, `legjobb_pont`, `legjobb_ido`, `pilla_hely`) VALUES
+(1, 'Nagy Ádám', 15, 6.25, 18, 6.1, 16, 6.15, 18, 6.1, 1),
+(2, 'Kovács Bence', 14, 7.01, 15, 6.88, 14, 6.95, 15, 6.88, 2),
+(3, 'Tóth Csaba', 19, 5.5, 20, 5.42, 18, 5.6, 20, 5.42, 3),
+(4, 'Szabó Dóra', 10, 8.12, 11, 7.99, 10, 8.05, 11, 7.99, 4),
+(5, 'Varga Ede', 17, 5.95, 16, 6.02, 17, 5.9, 17, 5.9, 5),
+(6, 'Kiss Fanni', 13, 7.5, 14, 7.45, 12, 7.6, 14, 7.45, 6),
+(7, 'Horváth Gábor', 20, 5.3, 21, 5.25, 20, 5.35, 21, 5.25, 7),
+(8, 'Lakatos Hédi', 15, 6.7, 15, 6.65, 16, 6.55, 16, 6.55, 8),
+(9, 'Molnár István', 11, 7.8, 12, 7.75, 13, 7.7, 13, 7.7, 9),
+(10, 'Juhász Júlia', 16, 6.35, 17, 6.3, 16, 6.4, 17, 6.3, 10);
 
 --
 -- Indexek a kiírt táblákhoz
 --
 
 --
--- A tábla indexei `attempt`
+-- A tábla indexei `versenyzok`
 --
-ALTER TABLE `attempt`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `competitor_id` (`competitor_id`);
-
---
--- A tábla indexei `competitor`
---
-ALTER TABLE `competitor`
+ALTER TABLE `versenyzok`
   ADD PRIMARY KEY (`id`);
-
---
--- A kiírt táblák AUTO_INCREMENT értéke
---
-
---
--- AUTO_INCREMENT a táblához `attempt`
---
-ALTER TABLE `attempt`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
-
---
--- AUTO_INCREMENT a táblához `competitor`
---
-ALTER TABLE `competitor`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- Megkötések a kiírt táblákhoz
---
-
---
--- Megkötések a táblához `attempt`
---
-ALTER TABLE `attempt`
-  ADD CONSTRAINT `attempt_ibfk_1` FOREIGN KEY (`competitor_id`) REFERENCES `competitor` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
